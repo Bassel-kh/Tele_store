d5c14f7f7c851667590f25f44f5fa9b052b61013
Bassel-kh <bassel.kh.svu@gmail.com>
2020-08-08 23:09:20 +0300

    #4 (test) add main_index page and login &&

--- resources/views/auth1/login.blade.php
+++ resources/views/auth1/login.blade.php
@@ -1,73 +1,28 @@
-@extends('layouts.app')
+<html lang="ar" dir="rtl">
+<head>
+    <title> Login 1</title>
+    <link rel="stylesheet" href="css/loginstyle.css">
+</head>
+<body>
+<div class="loginbox">
+    <img src="img/avatar.png" class="avatar" alt="">
+    <h1>تسجيل الدخول </h1>
+    <form action="">
+        <p> البريد الالكتروني</p>
+        <input type="text" placeholder="أدخل عنوان البريد الالكتروني">
+        <p>كلمة المرور </p>
+        <input type="password" placeholder="أدخل كلمة المرور">
+        <input  type="submit" value="تسجيل الدخول  ">
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+        <div class="form-check container">
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <label for="remember" class="lable"> تذكر كلمة المرور</label>
+            <input class="check-box-div" type="checkbox" id="remember" name="remember" >
         </div>
-    </div>
-@endsection
+
+        <a href="#" > هل نسيت كلمة المرور؟</a>
+        <a href="#" > لا تمتلك حساب؟</a>
+    </form>
+</div>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,180 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="ar" dir="rtl">
+<head>
+    <title>Telecom</title>
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/43af4b3caa.js" crossorigin="anonymous"></script>
 
-        <title>Laravel</title>
+</head>
+<body>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light  mynav ">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">الرئيسية  </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#services">خدماتنا</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">الأسعار</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#About">من نحن؟</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">تواصل معنا</a>
+            </li>
+            <li>
+                <input type="checkbox" class="checkbox" id="chk" />
+                <label class="label" for="chk">
+                    <i class="fas fa-moon"></i>
+                    <i class="fas fa-sun"></i>
+                    <div class="ball"></div>
+                </label>
+            </li>
+        </ul>
+        <div class="mr-auto navDiv2 ">
+            <ul class="navbar-nav">
+                <li class="nav-item " >
+                    <a class="nav-link"  href="{{ route('login') }}">تسجيل دخول</a>
+                </li>
+                <li class="nav-item" >
+                    <a class="nav-link" href="#">انشاء حساب</a>
+                </li>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!---------- banner section-------->
+<section id="banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="promo-title"> العراب للاتصالات</p>
+                <p> المركز الأول في تقديم الخدمات عن بعد
+                    <br>
+                    دفع فواتير الانترنيت من المنزل
+                    <br>
+                    تحويل رصيد ام تي ان وسيرياتيل
+                    <br>
+                    ولسا كمان وكمان اي شي بيخطر عبالك عنا
+                </p>
+                <div class="bdiv text-center ml-auto">
+                    <a id="log" class="nav-link "  href="{{ route('login') }}" >تسجيل دخول</a>
+                    <a id="sign" class="nav-link " href="#">انشاء حساب</a>
                 </div>
             </div>
+            <div class="col-md-6 text-center">
+                <img  src="img/home1.png" class="img-fluid" alt="">
+            </div>
         </div>
-    </body>
+    </div>
+</section>
+
+<!------------------------ services --------------------->
+<section id="services" >
+    <div class="container text-center">
+        <h1 class="title">خدماتنا</h1>
+        <div class="row ">
+            <div class="col-md-2"></div>
+
+            <div class="col-md-4 services">
+                <h4>متجر خاص للصفحة</h4>
+                <p>باللغة العربية الفصحى يوجد العديد من الخدمات لكي نقدمها تحويل رصيد ام تي ان وسيريتيل
+                    <br> دفع فواتير بوابات ADSL بالاضافة الى العديد والعديد
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img src="img/service1.png" class="img-fluid service-img" alt="">
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+        <div class="row ">
+            <div class="col-md-2"></div>
+            <div class="col-md-4">
+                <img src="img/service2.png" class="img-fluid service-img" alt="">
+            </div>
+            <div class="col-md-4 services">
+                <h4>متجر خاص للصفحة</h4>
+                <p>باللغة العربية الفصحى يوجد العديد من الخدمات لكي نقدمها تحويل رصيد ام تي ان وسيريتيل
+                    <br> دفع فواتير بوابات ADSL بالاضافة الى العديد والعديد
+                </p>
+            </div>
+
+            <div class="col-md-2"></div>
+        </div>
+        <a type="button" class="btn btn-primary" href="{{ route('payment_services') }}" > كل الخدمات </a>
+    </div>
+</section>
+<!------------------------- About ------------------------->
+<section id="About">
+    <div class="container text-center">
+        <h1 class="title">من نحن؟</h1>
+        <div class="row">
+            <div class=" text-center">
+                <p> من نحن ؟ سؤال يُسأل لنا كل يوم لا ادري من نحن ولا ماذا نفعل حبك حبك ما حبك تصطفل
+                    المهم عبي كم سطر بالعربي لحتى شوف التنسيق كيف رح يطلع
+                    يعني بصراحة أغبى شي ممكن تعملو لما بدك تحط اي نص وتوقف متل الحمار ما
+                    تعرف شو تكتب
+                    يعني بصراحة أغبى شي ممكن تعملو لما بدك تحط اي نص وتوقف متل الحمار ما
+                    يعني بصراحة أغبى شي ممكن تعملو لما بدك تحط اي نص وتوقف متل الحمار ما
+                    يعني بصراحة أغبى شي ممكن تعملو لما بدك تحط اي نص وتوقف متل الحمار ما
+                    يعني بصراحة أغبى شي ممكن تعملو لما بدك تحط اي نص وتوقف متل الحمار ما
+                    تعرف شو اللي بدك تكتبو المهم التنسيق طلع ظريف وضا متل ما هو وما عملي مشاكل .
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!------------------------------- contact ----------------------------->
+<section id="contact">
+    <div class="container text-center">
+        <h1 class="title">  تواصل معنا </h1>
+        <div class="row contact-icon">
+            <div class="col-md-4  ">
+                <i class="fas fa-map-marker-alt"></i>
+                <p class="text-center my-2">طرطوس - دوير الشيخ سعد - جانب المشفى</p>
+            </div>
+            <div class="col-md-4  ">
+                <i class="fas fa-phone-alt"></i>
+                <p class="text-center mt-2 mb-0"> 0999-888777</p>
+                <p class="text-center mt-0"> 0999-888777</p>
+            </div>
+            <div class="col-md-4">
+                <i class="fas fa-envelope"></i>
+                <p class="text-center my-2">Email : info@example.com</p>
+
+            </div>
+        </div>
+
+    </div>
+
+</section>
+<!------------------------------- footer ----------------------------->
+<section id="footer">
+    <div>
+        <svg class=" img-fluid" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#008080" fill-opacity="1"
+                  d="M0,160L40,176C80,192,160,224,240,213.3C320,203,400,149,480,133.3C560,117,640,139,720,170.7C800,203,880,245,960,234.7C1040,224,1120,160,1200,122.7C1280,85,1360,75,1400,69.3L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z">
+
+            </path>
+        </svg>
+    </div>
+    <div class="fdiv">
+        <p class="text-center"> Website Created By HYBA Team <br><br></p>
+    </div>
+</section>
+<script  src="js/main.js"></script>
+</body>
 </html>
